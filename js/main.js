501cf4a1d18a4f58237a28e01ed96a04484d6077
kudawoo2002 <kudawoo2002@gmail.com>
2024-04-16 07:15:35 +0000

    event, newsletter and footer added

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const navbarEl = document.querySelector(".navigation");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbarEl.classList.add("sticky");
+  } else {
+    navbarEl.classList.remove("sticky");
+  }
+});
