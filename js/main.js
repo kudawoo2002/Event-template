7b7721f8327e135a1ea207f0c8a3c6e8c81f555c
kudawoo2002 <kudawoo2002@gmail.com>
2024-04-20 10:27:38 +0000

    adding event part and timer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Navigation
 const navbarEl = document.querySelector(".navigation");
 
 window.addEventListener("scroll", () => {
@@ -7,3 +8,43 @@ window.addEventListener("scroll", () => {
     navbarEl.classList.remove("sticky");
   }
 });
+// End of Navigation
+
+// Event Time
+let hoursEl = document.querySelector(".hours");
+let minutesEl = document.querySelector(".minutes");
+let secondsEl = document.querySelector(".seconds");
+console.log(minutesEl);
+let minute = 0;
+let hour = 0;
+function getTime() {
+  const timeToday = new Date();
+  let seconds = timeToday.getSeconds();
+
+  if (seconds > 0) {
+    if (seconds < 10) {
+      secondsEl.textContent = `0${seconds}`;
+    } else {
+      secondsEl.textContent = seconds;
+    }
+  } else {
+    minute = minute + 1;
+    if (minute < 10) {
+      minutesEl.textContent = `0${minute}`;
+    } else {
+      minutesEl.textContent = minute;
+    }
+  }
+  if (minute >= 59) {
+    hour = hour + 1;
+    if (minute < 10) {
+      hoursEl.textContent = `0${hour}`;
+    } else {
+      hour.textContent = hour;
+    }
+  }
+}
+setInterval(() => {
+  getTime();
+}, 1000);
+// end f Event Time
